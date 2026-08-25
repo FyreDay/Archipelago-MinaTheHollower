756cb7e690a4bd7e37b41f41857fad277c62f3bb
FyreDay <fyredaymc@gmail.com>
2026-08-25 13:08:59 -0500

    remove extra image, fix SO West Wall logic, update backwaters image

--- worlds/mina_the_hollower/test/test_basic.py
+++ worlds/mina_the_hollower/test/test_basic.py
@@ -12,3 +12,15 @@ class TestBasic(MinaTestBase):
     def test_can_beat_game(self):
         self.collect_all_but([])
         self.assertBeatable(True)
+
+# class TestFromDict(MinaTestBase):
+#     options = {}
+#     world: MinaTheHollowerWorld
+#
+#     def test_can_call(self):
+#         entrance_rule = {"children":[{"args":{"count":1.0,"item_name":"Burrow"},"options":[],"rule":"Has"},{"args":{"count":1.0,"item_name":"Climb"},"options":[],"rule":"Has"}],"options":[],"rule":"And"}
+#
+#         rule = self.world.rule_from_dict(entrance_rule)
+#
+#         print(rule)
+
